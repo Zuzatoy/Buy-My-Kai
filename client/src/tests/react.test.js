2c2d2f1f7a0e389925fe49628ba7e2c758504914
Lauren Hart <laurenannehart@gmail.com>
2018-10-03 19:48:48 +1300

    added one react test

---
+++ client/src/tests/react.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {shallow, configure} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import {Addproduct} from '../components/Product/Addproduct'
+
+configure({adapter: new Adapter()})
+
+test('test runner runs', () => {
+  expect(true).toBeTruthy()
+})
+
+test('<Addproduct />', () => {
+  const expected = 'Add a Product'
+
+  const wrapper = shallow(<Addproduct />)
+  const actual = wrapper.find('h1').text()
+
+  expect(actual).toEqual(expected)
+})
